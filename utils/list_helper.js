f6a87e9e268616aa42cbb5d8a7960365d61fcad6
Henry Lin <henry.lin.hln@gmail.com>
2020-07-05 17:28:29 -0400

    Exercise 4.6 completed
    Completed without lodash (refactor later)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,14 +8,52 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+/* The function returns the author who has the largest amount of blogs
+The return value also contains the number of blogs the top author has */
 const favoriteBlog = (blogs) => {
   return blogs.reduce((favorite, blog) => {
     return (favorite.likes >= blog.likes ? favorite : blog)
   })
 }
 
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = []
+  blogs.forEach(blog => {
+    if (blogsByAuthor[blog.author] === undefined) {
+      blogsByAuthor[blog.author] = 1
+    } else {
+      blogsByAuthor[blog.author]++
+    }
+  })
+
+  let max = 0
+  const mostBlogs = {
+    "author": "none",
+    "blogs": 0
+  }
+
+  // key is name of author
+  // blogsByAuthor[key] is number of blogs
+  for (const key in blogsByAuthor) {
+    if (blogsByAuthor[key] > max) {
+      max = blogsByAuthor[key]
+      mostBlogs.author = key
+      mostBlogs.blogs = blogsByAuthor[key]
+    }
+  }
+
+  return mostBlogs
+}
+
+/* The function returns the author, whose blog posts have the largest amount of likes
+The return value also contains the total number of likes that the author has received: */
+const mostLikes = (blogs) => {
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
