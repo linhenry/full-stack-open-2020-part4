2506f561312743626204cb1db7c02868a4cf9019
Henry Lin <henry.lin.hln@gmail.com>
2020-07-06 18:57:04 -0400

    Exercise 4.10 completed
    Added test verifying HTTP POST request to /api/blogs, checks for title and length of blogs object after creating

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
+const { initialBlogs } = require('./test_helper')
 
 test('all blogs are returned as json', async () => {
   await api
@@ -19,8 +20,27 @@ test('all blogs are returned', async () => {
 
 test('blog has id property', async () => {
   const blogs = await helper.blogsInDb()
-  console.log('blogs', blogs)
   expect(blogs[0].id).toBeDefined()
+})
+
+test('a new blog is created', async () => {
+  const newBlog = {
+    "title": "Hello World Blog",
+    "author": "Henry Lin",
+    "url": "helloworldblog.com",
+    "likes": "15"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(initialBlogs.length + 1)
+  const titles = blogsAtEnd.map(blog => blog.title)
+  expect(titles).toContain('Hello World Blog')
 })
 
 afterAll(() => {
